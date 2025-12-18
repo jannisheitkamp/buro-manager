9a6e9a94d05e461c30bfd8a412b3b54ef28cb6cb
Jannis Heitkamp <jannis@Mac.fritz.box>
2025-12-18 10:54:39 +0100

    Fix missing Polls import

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Dashboard } from '@/pages/Dashboard';
 import { Calendar } from '@/pages/Calendar';
 import { Bookings } from '@/pages/Bookings';
 import { Board } from '@/pages/Board';
+import { Polls } from '@/pages/Polls';
 import { Directory } from '@/pages/Directory';
 import { ProfilePage } from '@/pages/ProfilePage';
 import { Toaster } from 'react-hot-toast';
